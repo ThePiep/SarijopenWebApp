4c4255f746ff1de29b09da6c368584e0bb1a368e
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2023-04-24 00:57:00 +0200

    Update packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+// const withPWA = require("next-pwa")({
+//   dest: "app",
+// });
+
 const nextConfig = {
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ["mysql2"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
