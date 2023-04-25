151262932ef1f2d41e9a1311712f51840dd2d87b
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2023-04-25 08:03:02 +0200

    Add getBewoners cached method

---
+++ util/bewoners.ts
@@ -0,0 +1,21 @@
+import sequelize from "@/lib/sequelize";
+import { bewoners } from "@/models/bewoners";
+import { bewonersCache } from "./cache";
+
+export const getBewoners = async (): Promise<bewoners[]> => {
+  const cachedBewoners = await bewonersCache.get("bewoners");
+  console.log("cachedBewoners", cachedBewoners ? cachedBewoners[0] : null);
+  if (cachedBewoners) {
+    console.log("cachedBewoners found");
+    return cachedBewoners;
+  } else {
+    bewoners.initModel(sequelize);
+    const result = await bewoners.findAll({
+      attributes: ["id", "naam", "kamer", "huidig", "bierlijst"],
+      order: [["id", "ASC"]],
+    });
+    bewonersCache.put("bewoners", result, 1000 * 60 * 30); // 30 minutes
+    console.log("new bewoners cache");
+    return result;
+  }
+};
