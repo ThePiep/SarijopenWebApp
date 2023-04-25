1e0ef91a7d914e70a31cfdcaf32164dd91a76306
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2023-04-25 08:03:59 +0200

    Fix sequelize

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,15 +1,13 @@
 import { Dialect, Sequelize } from "sequelize";
 import mysql from "mysql2";
-import { uitspraken } from "@/models/uitspraken";
-import { bewoners } from "@/models/bewoners";
 import { initModels } from "@/models/init-models";
 
 const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PWD,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT as Dialect,
+  dialect: 'mysql',
   dialectModule: mysql,
 });
 
