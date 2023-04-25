8189d46ec755a312b41a03f7023823c315ecf100
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2023-04-25 08:01:53 +0200

    Validate uitspraken POST endpoint with new schema

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -5,28 +5,28 @@ import dayjs from "dayjs";
 import { getServerSession } from "next-auth";
 import { getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
+import { nieuweUitspraakSchema } from "./schema";
 
 export async function POST(req: NextRequest) {
   const session = await getToken({ req });
-  console.log("session", session);
   if (!session) {
     return new Response("Unauthorized", { status: 401 });
   }
   uitspraken.initModel(sequelize);
   const obj: uitspraken = await req.json();
-  console.log("json", obj);
+  const validated = await nieuweUitspraakSchema.validate(obj);
 
-  if (
-    obj.uitspraak === "" ||
-    (obj.bewonerID === 0 && obj.gast === "") ||
-    (obj.bewonerID !== 0 && obj.gast !== "") ||
-    (obj.tegenbewonerID !== 0 && obj.tegengast !== "")
-  ) {
-    return new Response("Error: Incorrect input", { status: 400 });
+  if (!validated) {
+    return new Response("Incorrect input", { status: 400 });
   }
+
   const now = dayjs();
   const uitspraak = await uitspraken.create({
     ...obj,
+    bewonerID: validated.bewonerID ?? 0,
+    gast: validated.gast ?? "",
+    tegenbewonerID: validated.tegenbewonerID ?? 0,
+    tegengast: validated.tegengast ?? "",
     datum: now.format("YYYY-MM-DD"),
     tijd: now.format("HH:mm:ss"),
   });
