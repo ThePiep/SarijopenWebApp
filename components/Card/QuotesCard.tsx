0a3c9f88f769914070b41f170bcb56efe3908d3a
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2023-05-12 17:52:32 +0200

    Styling quotes card

--- components/Card/QuotesCard.tsx
+++ components/Card/QuotesCard.tsx
@@ -2,8 +2,6 @@ export const revalidate = 60;
 
 import Link from 'next/link';
 import { Card, CardProps } from './Card';
-import { FaBan } from 'react-icons/fa';
-import { getServerSession } from 'next-auth';
 import { uitspraken } from '@/models/uitspraken';
 import sequelize from '@/lib/sequelize';
 
@@ -20,7 +18,7 @@ export const QuotesCard = async ({ ...props }: Omit<CardProps, 'title'>) => {
       <Card {...props} title={'Citatenboek'}>
         <div className='flex justify-center items-center h-full '>
           <div className='text-center'>
-            <div className='text-3xl font-extrabold'>{count}</div>
+            <div className='text-3xl font-bold'>{count}</div>
             <div className='text-2xs mt-2'>
               Dwaze uitspraken sinds Oktober 2000
             </div>
